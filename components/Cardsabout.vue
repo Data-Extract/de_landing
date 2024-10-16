7efc696831be2126650b9fe29a912746df4a9366
Sara1227 <sara.angarita@dataextract.co>
2024-10-15 20:43:07 -0500

    Creacion de componente Cardsabout
    Se creo el componente Cardsabout con una imagen y un botón de redirección al inicio de la pagina.

---
+++ components/Cardsabout.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="image-container">
+
+        <v-img
+         :width="279"
+         aspect-ratio="16/9"
+         cover
+         src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+        </v-img>
+    </div>
+        <div class = "button-container">
+             <v-btn to= "/"> 
+             Inicio
+             </v-btn>
+             </div>
+
+</template> 
+
+<script setup></script>
+
+<style scoped>
+ .image-container {
+ display: flex;
+ justify-content: center; /* Centrar de manera horizontal */
+ align-items: center;/* Centrar verticalmente */
+ height : 60vh;  /* Altura de la pantalla del contenedor */
+}
+</style>
+
+<style scoped>
+.button-container {
+ display: flex;
+ justify-content: center; /* Centrar de manera horizontal */
+ align-items: center;/* Centrar verticalmente */
+ height : 20vh;  /* Altura de la pantalla del contenedor */
+}
+</style>
+
+
+
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <h1>
-      {{ $t("aboutTitle") }}
+      {{ $t("Acerca de nosotros") }}
     </h1>
+    <p> Somos un equipo dedicado a ofrecerte orientación experta y soporte práctico para potenciar tus decisiones.</p>
+    <Cardsabout/>
   </div>
 </template>
