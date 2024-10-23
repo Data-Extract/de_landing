a9d98b7c64b4e34b834bb7f09f71c3c3c8ae8f0b
Sara1227 <sara.angarita@dataextract.co>
2024-10-23 17:34:09 -0500

    State Management with Pinia in Nuxt.js

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,7 +1,37 @@
 <template>
-    <div>
-        <h1>
-            {{ $t("servicesTitle") }}
-        </h1>
-    </div>
+  <div>
+    <h1>
+      {{ $t("servicesTitle") }}
+    </h1>
+
+    <v-container class="containerp">
+      <v-row justify="center" class="mb-5">
+        <v-col cols="12">
+          <v-sheet
+            class="pa-4 contact-background text-white"
+            elevation="3"
+            height="50px"
+          >
+            <h2 class="font weight-bold">Contact Settings</h2>
+          </v-sheet>
+        </v-col>
+      </v-row>
+      <v-row justify="space-around">
+        <EmailContacts />
+        <SmsContact />
+        <PhoneContacts />
+        <CommentsNotification />
+      </v-row>
+    </v-container>
+  </div>
 </template>
+
+<style scoped>
+.containerp {
+  background-color: rgb(198, 216, 250);
+}
+
+.contact-background {
+  background-color: rgb(35, 35, 71);
+}
+</style>
