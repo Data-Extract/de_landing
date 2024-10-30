21f90d7a0c6531dc5a8981618dad027904be1d1f
Sara1227 <sara.angarita@dataextract.co>
2024-10-30 10:44:54 -0500

    Clean service page and create component

---
+++ components/GridServices.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+interface Servicio {
+  titulo: string;
+  subtitulo: string;
+  imgURL: string;
+}
+
+const props = defineProps<{
+  servicios: Servicio[];
+}>();
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="(servicio, index) in servicios"
+        :key="index"
+        cols="7"
+        sm="5"
+        md="4"
+      >
+        <v-card>
+          <v-img :src="servicio.imgURL" height="100"> </v-img>
+          <v-card-title>{{ servicio.titulo }}</v-card-title>
+          <v-card-subtitle>{{ servicio.subtitulo }}</v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
