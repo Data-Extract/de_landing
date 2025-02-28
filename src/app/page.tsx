a3ce4c8e91ca792dc58b9cdba97a24a73d5f7bbc
michaelAuditore <michael_v613@hotmail.com>
2025-02-27 21:23:53 -0500

    feat: implement navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/ui/navbar";
+
 export default function Home() {
   return (
     <main >
+      <Navbar />
       <h1>Hola mundo</h1>
     </main>
   );
---
+++ src/ui/navbar/mobile.tsx
@@ -0,0 +1,12 @@
+export default function MobileNavbar() {
+    return (
+        <nav className="block md:hidden">
+            <ul>
+                <li>Blog</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    )
+}
