a2a0b89dc1e59fa03eabef561b10c489f0cdf0c8
root <root@DiegoParada.localdomain>
2025-03-10 21:57:19 -0500

    feat: responsive navbar

--- src/app/ui/navbar/mobile.tsx
+++ src/app/ui/navbar/mobile.tsx
@@ -1,7 +1,10 @@
 export default function MobileNavbar() {
     return (
-        <nav className="block md:hidden">
-            <ul>
+        <nav className="flex md:hidden justify-between">
+            <div className="logo">
+                <p>Dataextract</p>
+            </div>
+            <ul className="flex space-x-6">
                 <li>Blog</li>
                 <li>Services</li>
                 <li>About</li>
